15c097820f0914e87b338e7f832e793306d1e7de
UtkarshChandra41 <utkarshchandra41@gmail.com>
2025-06-10 12:54:30 +0530

    learn cpp

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    cout << "My name is Utkarsh Chandra" << endl << "\n This is my first CPP program";
+
+}
---
+++ printingNumbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main () { 
+    cout << "4" << endl;   // string
+    cout << 5 << endl;      // numbers
+    cout << 4+5;
+    cout << 3*3;
+    cout << "3*3";
+}
