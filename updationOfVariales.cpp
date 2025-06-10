73746b8b22f0f9c057f0e6691e7be302ea089472
UtkarshChandra41 <utkarshchandra41@gmail.com>
2025-06-10 13:52:59 +0530

    start CPP

---
+++ updationOfVariales.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int x=5;
+    cout << x << endl;
+    x=8;
+    cout << x << endl;
+ 
+    float z=2.34;
+    cout << z << endl;
+}
---
+++ variales.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int  x;      //declaration
+    x=5;        //initialization
+
+    int y=29;  //both at once
+
+    cout << x;
+    cout << y;
+}
