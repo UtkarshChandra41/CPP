350a225803e000e874c9d2c00f0640602981b698
UtkarshChandra41 <utkarshchandra41@gmail.com>
2025-06-11 14:08:28 +0530

    debug the finit  run

--- squareOfANumber.cpp
+++ squareOfANumber.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main () {
-    int x;
-    cout << "Enter a number";
-    cin >> x ;
-    cout << "Squared of the number is : ";
-    cout << x*x ;
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Square: " << n * n << endl;
+    return 0;
 }
