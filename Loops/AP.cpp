e0497c15cc498f0f38cbdf4dcec6aa83ca85b40b
UtkarshChandra41 <utkarshchandra41@gmail.com>
2025-06-12 15:45:15 +0530

    loops

---
+++ Loops/AP.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // 1 3 5 7 9 .....
+    int n;
+    cout << " TO print N ODD terms" << endl;
+    cout << "Enter the value of N" << endl;
+    cin >> n;
+    for (int i=1 ; i<=2*n-1 ; i+=2){
+        cout << i << " ";
+    }
+}
