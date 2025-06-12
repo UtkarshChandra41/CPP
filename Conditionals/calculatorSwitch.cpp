3f70d95a7038a68d350fed03f4ca8197db56c1cf
UtkarshChandra41 <utkarshchandra41@gmail.com>
2025-06-12 13:55:59 +0530

    update conditionals

---
+++ Conditionals/calculatorSwitch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+ int n1;  //number 1
+ cin >> n1;
+ char op; // operator
+ int n2;; // number 2
+ cin >> n2;
+ switch (op){
+    case '+'
+    cout << n1+n2 << endl;
+    case '-';
+    cout << n1-n2 << endl;
+    case '/';
+    cout << n1/n2 << endl;
+    case '*';
+    cout << n1*n2 << endl;
+ }
+}
---
+++ Conditionals/divisibleBy5and3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int x;
+    cout << "Enter a number";
+    cin >> x;
+    if(x%5==0 && x%2==0)
+        {
+        cout << "DIVISIBLE BY 5 AND 3";
+        }
+        
+        else
+
+        cout << "NOT DIVISIBLE";
+    
+}
